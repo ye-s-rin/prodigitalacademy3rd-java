8ddd663d5c7213efcbbbb0495aa23d2fdabbef1e
ye-s-rin <pyr53540@gmail.com>
2024-04-05 08:10:00 +0900

    fix: 한국인, 미국인 -> 한국어, 영어

--- src/day02/Work.java
+++ src/day02/Work.java
@@ -3,11 +3,15 @@ package day02;
 public class Work {
     public static void main(String[] args) {
         Store store = new Store();
+
         Phone galaxy = new SamsungPhone();
         Phone iphone = new ApplePhone();
 
-        Person sony = new Korean("쏘니", galaxy);
-        Person beckham = new American("베컴", iphone);
+        Language korean = new Korean();
+        Language english = new English();
+
+        Person sony = new Person("쏘니", korean, galaxy);
+        Person beckham = new Person("베컴", english, iphone);
         
         store.perchase(sony);
         store.perchase(beckham);
@@ -28,50 +32,40 @@ class Store{
 
 class Person {
     private String name;
+    private Language language;
     private Phone phone;
     
-    Person(String name, Phone phone){
+    Person(String name, Language language, Phone phone){
         this.name = name;
+        this.language = language;
         this.phone = phone;
     }
     
     void turnOn(){
         System.out.printf("\n%s(이/가) 핸드폰을 켰습니다.\n", this.name);
+        language.say();
         phone.turnOn();
     }
+}
 
-    Phone getPhone(){
-        return phone;
-    }
-
-    String getName(){
-        return this.name;
+class Language {
+    void say(){
+        System.out.println("오버라이딩");
     }
 }
 
-class Korean extends Person{
-    Korean(String name, Phone phone){
-        super(name, phone);
-    }
-
-    void turnOn(){
-        System.out.printf("\n%s(이/가) 핸드폰을 켰습니다.\n", super.getName());
+class Korean extends Language{
+    void say(){
         System.out.println("야 내가 핸드폰 켜볼게");
-        super.getPhone().turnOn();
     }
 }
 
-class American extends Person{
-    American(String name, Phone phone){
-        super(name, phone);
-    }
-
-    void turnOn(){
-        System.out.printf("\n%s(이/가) 핸드폰을 켰습니다.\n", super.getName());
+class English extends Language{
+    void say(){
         System.out.println("turn on");
-        super.getPhone().turnOn();
     }
 }
+
 
 class Phone{
     String sound="띠롱";
