d83b41301599c213880ae2d4d2e1b4d6d8dd3c6e
ye-s-rin <pyr53540@gmail.com>
2024-04-12 09:53:42 +0900

    feat: 버튼과 라벨

--- src/day07/MainView.java
+++ src/day07/MainView.java
@@ -8,16 +8,26 @@ public class MainView extends JFrame {
     JTextField tf_num1 = new JTextField();
     JTextField tf_num2 = new JTextField();
 
+    // 더하기 버튼
+    JButton btn_plus = new JButton("더하기");
+
+    // 라벨 = 가변 이름표
+    JLabel la_result = new JLabel("결과 출력");
+
     MainView() {
 
-        setSize(500, 300);
-        setLayout(null);
+        setSize(300, 200); // 창 크기
+        setLayout(null); // 레이아웃 매니저 초기화
 
         tf_num1.setBounds(50, 50, 50, 40);
         tf_num2.setBounds(100, 50, 50, 40);
+        btn_plus.setBounds(150, 50, 80, 40);
+        la_result.setBounds(50, 100, 200, 40);
 
         add(tf_num1);
         add(tf_num2);
+        add(btn_plus);
+        add(la_result);
 
         // 창을 끄면, 프로그램 종료
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
