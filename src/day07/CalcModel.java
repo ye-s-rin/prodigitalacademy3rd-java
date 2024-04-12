e89fe3b95cf7f4d820df74b3ee719c9610983e54
ye-s-rin <pyr53540@gmail.com>
2024-04-12 10:00:57 +0900

    feat: model - plus

---
+++ src/day07/CalcModel.java
@@ -0,0 +1,12 @@
+package day07;
+
+/**
+ * DB, 로직: 데이터 처리
+ */
+public class CalcModel {
+
+    // 매개변수 정수 2개 return 합
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+}
